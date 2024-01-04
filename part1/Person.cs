646b8fad36fc95947780ab570e89d618d9af14e5
hossein-lotfaraghi <154427080+hossein-lotfaraghi@users.noreply.github.com>
2024-01-04 21:43:34 +0330

    Add files via upload

---
+++ part1/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    class Person
+    {
+        private string _name;
+        private string _family;
+        private string _job;
+        private string _sex;
+        //--------------------------------------------
+
+        public string Name
+        {
+
+            get { return _name; }
+            set { _name = value; }
+
+
+        }
+
+        public string Family
+        {
+
+            get { return _family; }
+            set { _family = value; }
+
+
+        }
+
+        public string Job
+        {
+
+            get { return _job; }
+            set { _job = value; }
+
+
+        }
+
+        public string Sex
+        {
+
+            get { return _sex; }
+            set { _sex = value; }
+
+
+        }
+
+
+
+    }
+}
---
+++ part1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // استعاره گرفتن از کلاس و تولید یک شیء جدید از کلاس person
+            Person hossein_info = new Person();
+            
+            
+            // تعریف و مقدار دهی به فیلد ها برای یک آبجکت یا یک شیء
+
+            hossein_info.Name = "hossein";
+            hossein_info.Family = "araghi";
+            hossein_info.Job = "Csharp Developer";
+            hossein_info.Sex = "man";
+
+            //خروجی گرفتن
+
+            Console.WriteLine("press any key to even . . . .\n");
+            Console.ReadKey();
+            Console.WriteLine($"\nyour name is {hossein_info.Name}\nypur family is {hossein_info.Family}\nyour job is {hossein_info.Job}\nyour sex is {hossein_info.Sex}\n");
+            Console.ReadKey();
+        }
+    }
+}
